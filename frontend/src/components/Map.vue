13f5c1fdf87e15501c8a25c2a5956f043d01db6d
jaeung <jaeung949@gmail.com>
2022-08-21 03:14:43 +0900

    on work

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -3,12 +3,20 @@
 </template>
 
 <script>
+// 필요 데이터 불러오기
+import mapData from '../assets/json/filtered_buildings_geojson.json'
+
 // import { Clock, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
+let smapleStartPoint = mapData.features[0].geometry.coordinates[0][0]
+smapleStartPoint = [parseFloat(smapleStartPoint[0]), parseFloat(smapleStartPoint[1])]
+// const smapleStartPoint = [100, 100]
+
 // 각 기본 렌더링 사항 정의
 const scene = new THREE.Scene()
+// 카메라 설정
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -20,6 +28,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 const light = new THREE.DirectionalLight('hsl(0, 100%, 100%)')
 const geometry = new THREE.BoxGeometry(1, 1, 1)
+geometry.translate(smapleStartPoint[0] + 1, smapleStartPoint[1] + 1, 0)
 const material = new THREE.MeshStandardMaterial({
   side: THREE.FrontSide,
   color: 'hsl(0, 100%, 50%)',
@@ -33,16 +42,26 @@ export default {
   name: 'Map',
   data: function () {
     return {
+      mapData: mapData
     }
   },
   created: function () {
     scene.add(camera)
     scene.add(light)
     scene.add(cube)
+
+    mapData.features.forEach((element) => {
+      this.makeBuilding(scene, element)
+      return false
+    })
+
     scene.add(axes)
     renderer.setSize(window.innerWidth, window.innerHeight)
-    light.position.set(0, 0, 10)
-    camera.position.z = 5
+    light.position.set(smapleStartPoint[0], smapleStartPoint[1], 10)
+
+    // 카메라 위치/방향 업데이트
+    camera.position.set(smapleStartPoint[0], smapleStartPoint[1], 10)
+    controls.target = new THREE.Vector3(smapleStartPoint[0], smapleStartPoint[1], 0)
     scene.background = new THREE.Color('hsl(0, 100%, 100%)')
     // controls.rotateSpeed = 1.0
     // controls.zoomSpeed = 5
@@ -59,6 +78,34 @@ export default {
       requestAnimationFrame(this.animate)
       renderer.render(scene, camera)
       controls.update()
+    },
+    makeBuilding: function (scene, data) {
+      if (data.geometry.type === 'Polygon' && data.properties.A10 && data.geometry.coordinates.length > 0) {
+        const coords = data.geometry.coordinates
+        const height = data.properties.A10
+
+        const shape = new THREE.Shape()
+        const firstCoord = coords.shift()
+        shape.moveTo(parseFloat(firstCoord[0]), parseFloat(firstCoord[1]))
+        coords.forEach(element => {
+          shape.lineTo(parseFloat(element[0]), parseFloat(element[1]))
+        })
+
+        const extrudeSettings = {
+          steps: 1,
+          depth: parseFloat(height),
+          bevelEnabled: false
+        }
+
+        const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
+        const material = new THREE.MeshStandardMaterial({
+          side: THREE.FrontSide,
+          color: 'hsl(0, 100%, 50%)',
+          wireframe: false
+        })
+        const mesh = new THREE.Mesh(geometry, material)
+        scene.add(mesh)
+      }
     }
   },
   computed: {
