3a2286a1388e1359b19fdd38d4fdf49d1b3f3e1a
jaeung <jaeung949@gmail.com>
2022-08-21 00:01:49 +0900

    three js by script init

--- frontend/src/components/Canvas.vue
+++ frontend/src/components/Canvas.vue
@@ -1,25 +1,88 @@
 <template>
-  <Renderer ref='renderer'>
-    <Camera :position='{ z: 10 }' />
-    <Scene>
-      <PointLight :position='{ y: 50, z: 50 }' />
-      <Box ref='box' > <!--:rotation='{ y: Math.PI / 4, z: Math.PI / 4 }' -->
-        <LambertMaterial />
-      </Box>
-    </Scene>
-  </Renderer>
+  <div ref='canvas'></div>
 </template>
 
 <script>
+// import { Clock, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
+import * as THREE from 'three'
+// import TrackballControls from 'three-trackballcontrols'
+// import {
+//     BloomEffect,
+//     EffectComposer,
+//     GlitchPass,
+//     EffectPass,
+//     RenderPass
+// } from 'postprocessing'
+const scene = new THREE.Scene()
+// const composer = new THREE.EffectComposer(new WebGLRenderer())
+// const effectPass = new THREE.EffectPass(camera, new BloomEffect())
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+})
+const light = new THREE.DirectionalLight('hsl(0, 100%, 100%)')
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshStandardMaterial({
+  side: THREE.FrontSide,
+  color: 'hsl(0, 100%, 50%)',
+  wireframe: false
+})
+const cube = new THREE.Mesh(geometry, material)
+const axes = new THREE.AxesHelper(5)
+const speed = 0.01
+// let controls = null
+
 export default {
-  mounted () {
-    const renderer = this.$refs.renderer
-    const box = this.$refs.box.mesh
-    renderer.onBeforeRender(() => {
-      box.rotation.x += 0.01
-    })
+  name: 'TheCanvas',
+  data: function () {
+    return {
+    }
+  },
+  created: function () {
+    scene.add(camera)
+    scene.add(light)
+    scene.add(cube)
+    scene.add(axes)
+    // renderer.setSize(window.innerWidth, window.innerHeight)
+    light.position.set(0, 0, 10)
+    camera.position.z = 5
+    scene.background = new THREE.Color('hsl(0, 100%, 100%)')
+    // controls = new TrackballControls(this.camera)
+    // this.controls.rotateSpeed = 1.0
+    // this.controls.zoomSpeed = 5
+    // this.controls.panSpeed = 0.8
+    // this.controls.noZoom = false
+    // this.controls.noPan = false
+    // this.controls.staticMoving = true
+    // this.controls.dynamicDampingFactor = 0.3
+  },
+  mounted: function () {
+    this.$refs.canvas.appendChild(renderer.domElement)
+    this.animate()
+  },
+  methods: {
+    animate: function () {
+      requestAnimationFrame(this.animate)
+      renderer.render(scene, camera)
+      cube.rotation.y += speed
+      // this.controls.update()
+    }
+  },
+  computed: {
+    rotate: function () {
+      if (this.speed === '') {
+        return 0
+      } else {
+        return this.speed
+      }
+    }
   }
 }
 </script>
 
-<style scoped></style>
+<style src='../assets/css/canvas.css'></style>
