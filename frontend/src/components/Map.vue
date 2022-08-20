70485dbba73b5a4be8a6aeea01697276fe55433d
jaeung <jaeung949@gmail.com>
2022-08-21 01:09:20 +0900

    del

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -47,8 +47,6 @@ export default {
     // controls.rotateSpeed = 1.0
     // controls.zoomSpeed = 5
     // controls.panSpeed = 0.8
-    // controls.noZoom = false
-    // controls.noPan = false
     // controls.staticMoving = true
     // controls.dynamicDampingFactor = 0.3
   },
