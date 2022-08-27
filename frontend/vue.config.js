ed955a4be6918309044e422d6b7838f88a5e66ca
jaeung <jaeung@spacewalk.tech>
2022-08-27 22:20:52 +0900

    enable https

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,5 +4,12 @@ module.exports = {
   outputDir: path.resolve(__dirname, "../docs"),
   publicPath: process.env.NODE_ENV === 'production'
     ? '/site-analysis/'
-    : '/'
+    : '/',
+  devServer: {
+    // open: process.platform === 'darwin',
+    // host: '0.0.0.0',
+    // port: 8085, // CHANGE YOUR PORT HERE!
+    // hotOnly: false,
+    https: true,
+  },
 }
