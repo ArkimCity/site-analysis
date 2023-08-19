a90fb1d5f1eb083f6e44e20c014dae28b80338d3
Jaeung Kim <jaeung@spacewalk.tech>
2023-08-19 18:47:14 +0900

    Use sidebar with z-index
    Type: Fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <!-- <SideBar id="sidebar"/> -->
+    <SideBar id="sidebar"/>
     <router-view/>
   </div>
 </template>
 
 <script>
-// import SideBar from './components/SideBar.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   components: {
-    // SideBar
+    SideBar
   }
 }
 </script>
@@ -34,5 +34,18 @@ code {
 #app {
   width: 100%;
   height: 100vh;
+}
+
+#sidebar {
+  position: absolute;
+  background-color: aliceblue;
+  height: 90vh;
+  width: 300px;
+  left: 50px;
+  top: 50px;
+  border-radius: 10px 10px 10px 10px;
+  opacity: 0.9;
+  border: 4px solid #000000; /* 2px width, solid style, and black color */
+  z-index: 2;
 }
 </style>
